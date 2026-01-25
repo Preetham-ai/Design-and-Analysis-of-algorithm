f7b51340431dbc6289cb6dde8361255c090942e1
Preetham-ai <ch.sc.u4cse24139@ch.students.amrita.edu>
2026-01-25 23:53:38 +0530

    Add SumofNNaturalsquares.c program
    Implement a program to calculate the sum of squares of the first n natural numbers.

---
+++ Week-1/C Files/SumofNNaturalsquares.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int n;
+int sum=0;
+printf("CH.SC.U4CSE24139\n");
+printf("Enter value of n:");
+scanf("%d",&n);
+for(int i=1;i<n+1;i++){
+sum+=i*i;
+}
+printf("The sum of squares of first %d natural numbers is %d\n",n,sum);
+}
+/*Space Complexity O(1) 2 variables
+Justification:
+In main(): int variables - n,sum
+so the worst case is O(1)
+space used 8 bytes
+*/
